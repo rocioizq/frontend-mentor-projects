d03f05bf1315a98ac8f967f673b4bb75910553c9
rocioizq <rocioizquierdo98@gmail.com>
2025-04-17 21:53:35 +0200

    react version was added

---
+++ newbie/3-column-preview-card-component-main-main/react-versionn/src/App.jsx
@@ -0,0 +1,55 @@
+import "./App.css"
+
+const cars = [
+  {
+    id: 1,
+    type: "Sedans",
+    description:
+      "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+    button: "Learn more",
+    className: "sedan",
+    image:
+      "https://3-column-preview-card-component-main.vercel.app/images/icon-sedans.svg",
+  },
+  {
+    id: 2,
+    type: "Suvs",
+    description:
+      "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
+    button: "Learn more",
+    className: "suv",
+    image:
+      "https://3-column-preview-card-component-main.vercel.app/images/icon-suvs.svg",
+  },
+  {
+    id: 3,
+    type: "Luxury",
+    description:
+      "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+    button: "Learn more",
+    className: "luxury",
+    image:
+      "https://3-column-preview-card-component-main.vercel.app/images/icon-luxury.svg",
+  },
+];
+
+function CarsCard({ image, type, description, button, className }) {
+  return (
+    <div className={`card ${className}`}>
+      <img src={image} alt={type} />
+      <h2>{type}</h2>
+      <p>{description}</p>
+      <button>{button}</button>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="container">
+      {cars.map((car) => (
+        <CarsCard key={car.id} {...car} />
+      ))}
+    </div>
+  );
+}
